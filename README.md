22383518d567cebfc882333f96d11bfdc6c6729f
markio00 <marco.marchiante@gmail.com>
2022-05-14 13:38:41 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# MTSS-Assignment2
+Assignment 2 di MTSS 21/22
---
+++ src/main/java/it/unipd/mtss/App.java
@@ -0,0 +1,18 @@
+////////////////////////////////////////////////////////////////////
+// Marco Marchiante 1222397
+// Michele Bettin 1216735
+////////////////////////////////////////////////////////////////////
+
+package it.unipd.mtss;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
