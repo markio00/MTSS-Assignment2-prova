52b7278a900e8cbabe65c5a4e82cf4052e22e130
roranz <ems.roranz@gmail.com>
2022-05-19 18:25:24 +0200

    refactor init

--- src/main/java/it/unipd/mtss/business/ConcreteBill.java
+++ src/main/java/it/unipd/mtss/business/ConcreteBill.java
@@ -5,11 +5,6 @@
 
 package it.unipd.mtss.business;
 
-import it.unipd.mtss.model.User;
-import it.unipd.mtss.business.exception.BillException;
-import it.unipd.mtss.model.EItem;
-import it.unipd.mtss.model.ItemType;
-
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
@@ -17,22 +12,28 @@ import java.util.List;
 import java.util.Random;
 import java.util.Vector;
 
+import it.unipd.mtss.business.exception.BillException;
+import it.unipd.mtss.model.EItem;
+import it.unipd.mtss.model.ItemType;
+import it.unipd.mtss.model.User;
+
 public class ConcreteBill implements Bill{
     
     static final int presents = 10;
     static Vector<User> luckyUsers = new Vector<User>(10);
-
-    public double getOrderPrice(List<EItem> itemsOrdered, User user, 
-    LocalTime date) throws BillException {
+    
+    public double getOrderPrice(final List<EItem> itemsOrdered, 
+    final User user, 
+    final LocalTime date) throws BillException {
         
         max30Products(itemsOrdered);
-
+        
         if(lottery(user, date)){
             return 0;
         }
         
-        double total = getTotalPrice(itemsOrdered);
-
+        final double total = getTotalPrice(itemsOrdered);
+        
         return total - 
         getDiscount5Processors(itemsOrdered) - 
         getDiscount10Mouse(itemsOrdered) -
@@ -40,19 +41,19 @@ public class ConcreteBill implements Bill{
         get100Discount(total) +
         getCommission2(total);
     }
-
-    public boolean lottery(User user, LocalTime currentTime) {
-
-        int time = Integer.parseInt(DateTimeFormatter.ofPattern("HHmm").
+    
+    private boolean lottery(final User user, final LocalTime currentTime) {
+        
+        final int time = Integer.parseInt(DateTimeFormatter.ofPattern("HHmm").
         format(currentTime));
         
         if(time >= 1800 && time <= 1900){
-                
+            
             if(!luckyUsers.contains(user) && presents > luckyUsers.size() &&
             user.getBirthDate().compareTo(LocalDate.now().minusYears(18)) > 0){
-
-                Random rand = new Random();
-                int randomino = rand.nextInt(10); //0-9
+                
+                final Random rand = new Random();
+                final int randomino = rand.nextInt(10); //0-9
                 //10%
                 if(randomino == 0){
                     luckyUsers.add(user);
@@ -67,68 +68,69 @@ public class ConcreteBill implements Bill{
         return false;
         
     }
-
-    public double getTotalPrice(List<EItem> itemsOrdered) {
-
+    
+    private double getTotalPrice(final List<EItem> itemsOrdered) {
+        
         double tot = 0;
-
-        for(EItem item : itemsOrdered) {
+        
+        for(final EItem item : itemsOrdered) {
             tot += item.getPrice();
         }
-
+        
         return tot;
     }
-
-    public double getDiscount5Processors(List<EItem> itemsOrdered) {
+    
+    private double getDiscount5Processors(final List<EItem> itemsOrdered) {
         int processorsFound = 0;
         double minPrice = Double.MAX_VALUE;
-
-        for(EItem item : itemsOrdered){
+        
+        for(final EItem item : itemsOrdered){
             if(item.getItemType() == ItemType.Processor){
                 minPrice = Math.min(minPrice, item.getPrice());
                 ++processorsFound;
             }
         }
-
+        
         if(processorsFound >= 5){
             return minPrice/2;
         }
         
         return 0;
     }
-
-    public double getDiscount10Mouse(List<EItem> itemsOrdered) {
+    
+    private double getDiscount10Mouse(final List<EItem> itemsOrdered) {
         int mouseFound = 0;
         double minPrice = Double.MAX_VALUE;
-
-        for(EItem item : itemsOrdered){
+        
+        for(final EItem item : itemsOrdered){
             if(item.getItemType() == ItemType.Mouse){
                 minPrice = Math.min(minPrice, item.getPrice());
                 ++mouseFound;
             }
         }
-
+        
         if(mouseFound > 10){
             return minPrice;
         }
         
         return 0;
     }
-
-    public double get100Discount(double totalPrice)  {
+    
+    private double get100Discount(final double totalPrice)  {
         if(totalPrice > 1000) {
             return totalPrice/10;
         }
-
+        
         return 0;
     }
-
-    public double getDiscountSameMouseKeyboards(List<EItem> itemsOrdered) {
+    
+    private double getDiscountSameMouseKeyboards
+    (final List<EItem> itemsOrdered) {
         
         int matchFound = 0;
         double minPrice = Double.MAX_VALUE;
-
-        for(EItem item : itemsOrdered){
+        
+        for(final EItem item : itemsOrdered){
             if(item.getItemType() == ItemType.Mouse){
                 minPrice = Math.min(minPrice, item.getPrice());
                 ++matchFound;
@@ -138,36 +140,32 @@ public class ConcreteBill implements Bill{
                 --matchFound;
             }
         }
-
+        
         if(matchFound == 0 && !itemsOrdered.isEmpty()){
             return minPrice/2;
         }
         
         return 0;
     }
-
-    public double getCommission2(double totalPrice) {
+    
+    private double getCommission2(final double totalPrice) {
         if(totalPrice < 10) {
             return 2.0;
         }
-
+        
         return 0;
     }
-    public void max30Products(List<EItem> itemsOrdered) throws BillException{
+    private void max30Products(final List<EItem> itemsOrdered) 
+    throws BillException{
         // Act
-        int count = itemsOrdered.size();
+        final int count = itemsOrdered.size();
         // Assert
-
+        
         if (count > 30){
             throw new BillException("Mannaggia");
         }
         
     }
     
-
-    public double getOrderPrice(List<EItem> itemsOrdered, User user) 
-    throws BillException {
-        return getOrderPrice(itemsOrdered, user, LocalTime.of(15, 00, 00));
-    }
-
+    
 }
--- src/test/java/it/unipd/mtss/business/ConcreteBillTest.java
+++ src/test/java/it/unipd/mtss/business/ConcreteBillTest.java
@@ -1,230 +1,34 @@
 package it.unipd.mtss.business;
 
-import it.unipd.mtss.model.User;
-import it.unipd.mtss.business.exception.BillException;
-import it.unipd.mtss.model.EItem;
-import it.unipd.mtss.model.ItemType;
-
-import java.util.Vector;
 import java.time.LocalDate;
-import java.time.LocalTime;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import java.util.Vector;
 
 import org.junit.Before;
-import org.junit.Test;
+
+import it.unipd.mtss.model.EItem;
+import it.unipd.mtss.model.ItemType;
+import it.unipd.mtss.model.User;
 
 public class ConcreteBillTest {
-
+    
     private ConcreteBill bill;
     private User user;
     private Vector<EItem> list;
-
+    
     @Before
     public void testDataSetup() {
         // Arrange
         this.bill = new ConcreteBill();
-
+        
         this.user = new User("user1", "Mario", "Rossi", LocalDate.of(2010,04,19));
-
+        
         this.list = new Vector<EItem>();
         this.list.add(new EItem(ItemType.Processor, "R7 5700X", 300.00));
         this.list.add(new EItem(ItemType.Processor, "R5 5600X", 200.00));
-        this.list.add(new EItem(ItemType.Processor, "R7 5700X", 300.00));
-        this.list.add(new EItem(ItemType.Processor, "R5 5600X", 200.00));
-        this.list.add(new EItem(ItemType.Processor, "R7 5700X", 300.00));
-        
-        this.list.add(new EItem(ItemType.Mouse, "G502", 45.99));
-        this.list.add(new EItem(ItemType.Mouse, "crappy", 10));
-        this.list.add(new EItem(ItemType.Mouse, "crappy", 10));
-        this.list.add(new EItem(ItemType.Mouse, "crappy", 10));
-        this.list.add(new EItem(ItemType.Mouse, "crappy", 10));
-        this.list.add(new EItem(ItemType.Mouse, "crappy", 10));
-        this.list.add(new EItem(ItemType.Mouse, "crappy", 10));
-        this.list.add(new EItem(ItemType.Mouse, "crappy", 10));
-        this.list.add(new EItem(ItemType.Mouse, "crappy", 10));
-        this.list.add(new EItem(ItemType.Mouse, "crappy", 10));
         this.list.add(new EItem(ItemType.Mouse, "crappy", 10));
         this.list.add(new EItem(ItemType.Keyboard, "Ozone Stike Pro", 100));
-        this.list.add(new EItem(ItemType.Keyboard, "crappy", 20));
-        this.list.add(new EItem(ItemType.Keyboard, "crappy", 20));
-        this.list.add(new EItem(ItemType.Keyboard, "crappy", 20));
-        this.list.add(new EItem(ItemType.Keyboard, "crappy", 20));
-        this.list.add(new EItem(ItemType.Keyboard, "crappy", 20));
-        this.list.add(new EItem(ItemType.Keyboard, "crappy", 20));
-        this.list.add(new EItem(ItemType.Keyboard, "crappy", 20));
-        this.list.add(new EItem(ItemType.Keyboard, "crappy", 20));
-        this.list.add(new EItem(ItemType.Keyboard, "crappy", 20));
-        this.list.add(new EItem(ItemType.Keyboard, "super crappy", 5));
-        
-    }
 
-    @Test
-    public void testGetOrderPrice() {
-        double price = Double.MAX_VALUE;
-        try {
-            // Act
-            price = bill.getOrderPrice(list, user);
-
-            // Assert
-            assertEquals(price, 1445.391, 0);
-            price = bill.getOrderPrice(list, user, LocalTime.of(23, 12, 00));
-            assertEquals(price, 1445.391, 0);
-            price = bill.getOrderPrice(new Vector<EItem>(), user);
-            assertEquals(2, price, 0);
-            price = Double.MAX_VALUE;
-            for(int i = 0; i < 100; ++i){
-                price = Math.min(price, bill.getOrderPrice(list, user, LocalTime.of(18, 12, 00)));
-            }
-            assertEquals(0, price, 0);
-        }catch (BillException e) {
-            e.printStackTrace();
-            fail("Carrello ciccione");
-        }               
-    }
-
-    @Test
-    public void testGetTotalPrice() {
-        // Act
-        double tot = bill.getTotalPrice(list);
-
-        // Assert
-        assertEquals(tot, 1730.99, 0);
-
-        tot = bill.getTotalPrice(new Vector<EItem>());
-        assertEquals(0, tot, 0);
-    }
-
-    @Test
-    public void testGetDiscount5Processors() {
-        // Act
-        double discount = bill.getDiscount5Processors(list);
-
-        // Assert
-        assertEquals(100, discount,  0);
-
-        discount = bill.getDiscount5Processors(new Vector<EItem>());
-        assertEquals(0, discount, 0);
-    }
-
-    @Test
-    public void testGetDiscount10Mouse() {
-        // Act
-        double discount = bill.getDiscount10Mouse(list);
-
-        // Assert
-        assertEquals(10, discount,  0);
-
-        discount = bill.getDiscount10Mouse(new Vector<EItem>());
-        assertEquals(0, discount, 0);
-    }
-
-    @Test
-    public void testGet1000Discount() {
-        // Act
-        double tot = bill.getTotalPrice(list);
-        double discount = bill.get100Discount( tot);
-
-        // Assert
-        assertEquals(173.099, discount, 0);
-
-        assertEquals(0, bill.get100Discount(0), 0);
     }
     
-    @Test
-    public void testGetDiscountSameMouseKeyboards() {
-        // Act
-        double discount = bill.getDiscountSameMouseKeyboards(list);
-
-        // Assert
-        assertEquals(2.5, discount,  0);
-
-        discount = bill.getDiscountSameMouseKeyboards(new Vector<EItem>());
-        assertEquals(0, discount, 0);
-
-        Vector<EItem> v = new Vector<>(list);
-        v.add(new EItem(ItemType.Keyboard, "crappy", 20));
-        discount = bill.getDiscountSameMouseKeyboards(v);
-        assertEquals(0, discount, 0);
-    }
-
-    @Test
-    public void testGetCommission2() {
-        // Act 
-        double discountLT = bill.getCommission2(9);
-        double discountEQ = bill.getCommission2(10);
-        double discountGT = bill.getCommission2(11);
-
-        // Assert
-        assertEquals(2, discountLT, 0);
-        assertEquals(0, discountEQ, 0);
-        assertEquals(0, discountGT, 0);
-    }
-
-    @Test
-    public void testMax30Products(){
-        try {
-            bill.max30Products(list);
-        } catch (BillException e) {
-            assertEquals(e.getMessage(), "Mannaggia");
-        }
-        
-        try {
-            bill.max30Products(new Vector<EItem>());
-        } catch (BillException e) {
-            assertEquals(e.getMessage(), "Mannaggia");
-        }
-
-        Vector<EItem> test = new Vector<EItem>();
-        for (int i = 0; i < 50; ++i){
-            test.add(new EItem(ItemType.Processor, "test", 1));
-        }
-
-        try {
-            bill.max30Products(test);
-        } catch (BillException e) {
-            assertEquals(e.getMessage(), "Mannaggia");
-        }
-    }
-    @Test
-    public void testLottery(){
-        boolean result = true;
-        for(int i = 0; i<100; ++i){
-            result = result && bill.lottery(user,LocalTime.of(17, 05, 00));
-        }
-        assertEquals(result, false);
-
-
-
-        result = true;
-        for(int i = 0; i<50; ++i){
-            result = result && bill.lottery(new User("robo", "name", "surrname", 
-                    LocalDate.of(1990, 01, 01)),LocalTime.of(17, 05, 00));
-        }
-        assertEquals(result, false);
-
-                result = true;
-        for(int i = 0; i<50; ++i){
-            result = result && bill.lottery(new User("robo", "name", "surrname", 
-                    LocalDate.of(1990, 01, 01)),LocalTime.of(18, 05, 00));
-        }
-        assertEquals(result, false);
-
-        result = false;
-        for(int i = 0; i<50; ++i){
-            result = result || bill.lottery(new User("robo", "name", "surrname", 
-                    LocalDate.of(2012, 01, 01)),LocalTime.of(18, 05, 00));
-        }
-        assertEquals(result, true);
-
-        result = false;
-        ConcreteBill.luckyUsers.clear();
-        for(int i = 0; i<10; ++i){
-            ConcreteBill.luckyUsers.add(user);
-        }
-        result = bill.lottery(new User("robo", "name", "surrname", 
-                LocalDate.of(1990, 01, 01)),LocalTime.of(18, 05, 00));
-        assertEquals(result, false);
-    }
+    
 }
