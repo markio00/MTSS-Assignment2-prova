e5c450f76508bdd5c3b1ab56b98f2e8586246a87
Markio <marco.marchiante@gmail.com>
2022-05-19 17:53:08 +0000

    test cart size 30

--- src/test/java/it/unipd/mtss/business/ConcreteBillTest.java
+++ src/test/java/it/unipd/mtss/business/ConcreteBillTest.java
@@ -26,18 +26,21 @@ public class ConcreteBillTest {
     public void testEmptyOrder() {
         this.bill = new ConcreteBill();
 
+        // Set time of purchase out of range for random gifted order
+        LocalTime time = LocalTime.of(1, 0, 0);
+
         this.user = new User("user1", "Mario", "Rossi", LocalDate.of(2000, 04, 19));
         this.list = new Vector<EItem>();
 
         try {
 
-            double price = bill.getOrderPrice(list, user, LocalTime.of(1, 0, 0));
+            double price = bill.getOrderPrice(list, user, time);
     
             assertEquals(2, price, 0);
 
         } catch(BillException ex) {
             ex.printStackTrace();
-            fail("Carrello superiore a 3 elementi");
+            fail("Carrello superiore a 30 elementi");
         }
     }
 
@@ -45,6 +48,9 @@ public class ConcreteBillTest {
     @Test
     public void testTotalPriceSumWithoutCaveats() {
         this.bill = new ConcreteBill();
+
+        // Set time of purchase out of range for random gifted order
+        LocalTime time = LocalTime.of(1, 0, 0);
 
         this.user = new User("user1", "Mario", "Rossi", LocalDate.of(2000, 04, 19));
         this.list = new Vector<EItem>();
@@ -54,20 +60,59 @@ public class ConcreteBillTest {
 
         try {
 
-            double price = bill.getOrderPrice(list, user, LocalTime.of(1, 0, 0));
+            double price = bill.getOrderPrice(list, user, time);
     
             assertEquals(175.99, price, 0);
 
         } catch(BillException ex) {
             ex.printStackTrace();
-            fail("Carrello superiore a 3 elementi");
+            fail("Carrello superiore a 30 elementi");
         }
     }
+    
+    @Test
+    public void testDiscount5Processors(){
 
+        try {
+            bill = new ConcreteBill();
+            user = new User("user1", "Mario", "Rossi", LocalDate.of(2000,04,19));
+            list = new Vector<EItem>();
+            // Set time of purchase out of range for random gifted order
+            LocalTime time = LocalTime.of(10, 0, 0);
+            
+            list.add(new EItem(ItemType.Processor, "R7 5700X", 100.00));
+            list.add(new EItem(ItemType.Processor, "R7 5700X", 100.00));
 
+            assertEquals(200, bill.getOrderPrice(list, user, time),0);
+
+            list.add(new EItem(ItemType.Processor, "R7 5700X", 100.00));
+            list.add(new EItem(ItemType.Processor, "R7 5700X", 100.00));
+            list.add(new EItem(ItemType.Processor, "R7 5700X", 100.00));
+            
+            assertEquals(450, bill.getOrderPrice(list, user, time),0);
+
+            list.add(new EItem(ItemType.Processor, "Celeron", 30.00));
+            
+            assertEquals(515, bill.getOrderPrice(list, user, time),0);
+            
+            list.add(new EItem(ItemType.Motherboard, "b310", 30.00));
+            
+            assertEquals(545, bill.getOrderPrice(list, user, time),0);
+            
+            
+        } catch (BillException e) {
+            e.printStackTrace();
+            fail("Carrello superiore a 30 elementi");
+        }
+    }
+    
+    
     @Test
     public void giftCheapestMouseIfMoreThan10() {
         this.bill = new ConcreteBill();
+
+        // Set time of purchase out of range for random gifted order
+        LocalTime time = LocalTime.of(1, 0, 0);
 
         this.user = new User("user1", "Mario", "Rossi", LocalDate.of(2000, 04, 19));
         this.list = new Vector<EItem>();
@@ -86,53 +131,112 @@ public class ConcreteBillTest {
 
         try {
 
-            double price = bill.getOrderPrice(list, user, LocalTime.of(1, 0, 0));
+            double price = bill.getOrderPrice(list, user, time);
     
             assertEquals(103.00, price, 0);
 
             // do not gift if less os equal than 10 mouses in order
             this.list.add(new EItem(ItemType.Mouse, "G502", 10.00));
-            price = bill.getOrderPrice(list, user, LocalTime.of(1, 0, 0));
+            price = bill.getOrderPrice(list, user, time);
 
             assertEquals(105.00, price, 0);
 
         } catch(BillException ex) {
             ex.printStackTrace();
-            fail("Carrello superiore a 3 elementi");
+            fail("Carrello superiore a 30 elementi");
         }
     }
 
     @Test
-    public void testDiscount5Processors(){
+    public void discount10percentIFTotalOver1000() {
+        this.bill = new ConcreteBill();
+
+        // Set time of purchase out of range for random gifted order
+        LocalTime time = LocalTime.of(1, 0, 0);
+
+        this.user = new User("user1", "Mario", "Rossi", LocalDate.of(2000, 04, 19));
+        this.list = new Vector<EItem>();
+
+        this.list.add(new EItem(ItemType.Processor, "R9 5950X", 500.00));
+        this.list.add(new EItem(ItemType.Processor, "i9 12900K", 500.00));
+
         try {
-            bill = new ConcreteBill();
-            user = new User("user1", "Mario", "Rossi", LocalDate.of(2000,04,19));
-            list = new Vector<EItem>();
-            LocalTime time = LocalTime.of(10, 0, 0);
-            
-            list.add(new EItem(ItemType.Processor, "R7 5700X", 100.00));
-            list.add(new EItem(ItemType.Processor, "R7 5700X", 100.00));
 
-            assertEquals(200, bill.getOrderPrice(list, user, time),0);
+            double price = bill.getOrderPrice(list, user, time);
+    
+            assertEquals(1000.00, price, 0);
 
-            list.add(new EItem(ItemType.Processor, "R7 5700X", 100.00));
-            list.add(new EItem(ItemType.Processor, "R7 5700X", 100.00));
-            list.add(new EItem(ItemType.Processor, "R7 5700X", 100.00));
+            // chacking critical bound
+            this.list.add(new EItem(ItemType.Mouse, "G502", 0.01));
+            price = bill.getOrderPrice(list, user, time);
 
-            assertEquals(450, bill.getOrderPrice(list, user, time),0);
+            assertEquals(900.009, price, 0);
 
-            list.add(new EItem(ItemType.Processor, "Celeron", 30.00));
-
-            assertEquals(515, bill.getOrderPrice(list, user, time),0);
-
-            list.add(new EItem(ItemType.Motherboard, "b310", 30.00));
-
-            assertEquals(545, bill.getOrderPrice(list, user, time),0);
-
-
-        } catch (BillException e) {
-            e.printStackTrace();
+        } catch(BillException ex) {
+            ex.printStackTrace();
             fail("Carrello superiore a 30 elementi");
         }
+        
     }
+
+
+    @Test
+    public void maxOrderSize30() {
+        this.bill = new ConcreteBill();
+
+        // Set time of purchase out of range for random gifted order
+        LocalTime time = LocalTime.of(1, 0, 0);
+
+        this.user = new User("user1", "Mario", "Rossi", LocalDate.of(2000, 04, 19));
+        this.list = new Vector<EItem>();
+
+        this.list.add(new EItem(ItemType.Motherboard, "article1", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article2", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article3", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article4", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article5", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article6", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article7", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article8", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article9", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article10", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article11", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article12", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article13", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article14", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article15", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article16", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article17", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article18", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article19", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article20", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article21", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article22", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article23", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article24", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article25", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article26", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article27", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article28", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article29", 1.00));
+        this.list.add(new EItem(ItemType.Motherboard, "article30", 1.00));
+
+        try {
+
+            double price = bill.getOrderPrice(list, user, time);
+    
+            assertEquals(30, price, 0);
+
+            // chacking critical bound
+            this.list.add(new EItem(ItemType.Motherboard, "article31", 1.00));
+            price = bill.getOrderPrice(list, user, time);
+            
+            fail("Mode than 30 items are not allowed for one order");
+
+        } catch(BillException ex) {
+            ex.printStackTrace();
+            assertTrue(true);
+        }
+    }
+
 }
